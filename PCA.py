6ac546f8d51ff3bdb304c199ce06467d8762e1a7
Panagiotis <p.krokidas@gmail.com>
2023-10-03 10:30:09 +0300

    PCA.py: debug (not complete)
    Co-authored-by: George Giannakopoulos <ggianna@scify.org>

--- PCA.py
+++ PCA.py
@@ -42,89 +42,74 @@ df2.type.unique()
 TypeList = df2.type.unique()
 TypeList
 
+# Provided dictionaries
+linker_length1 = {
+    3.66: {'linker_mass1': 83, 'σ_1': 0.325, 'e_1': 0.7112, 'linker_length2': 3.66, 'linker_mass2': 83, 'σ_2': 0.325,
+        'e_2': 0.7112, 'linker_length3': 3.66, 'linker_mass3': 83, 'σ_3': 0.325, 'e_3': 0.7112},
+    4.438: {'linker_mass1': 81, 'σ_1': 0.25, 'e_1': 0.0627, 'linker_length2': 4.438, 'linker_mass2': 81, 'σ_2': 0.25,
+            'e_2': 0.0627, 'linker_length3': 4.438, 'linker_mass3': 81, 'σ_3': 0.25, 'e_3': 0.0627},
+    4.86: {'linker_mass1': 101.98, 'σ_1': 0.285, 'e_1': 0.255, 'linker_length2': 4.86, 'linker_mass2': 101.98, 'σ_2': 0.285,
+        'e_2': 0.255, 'linker_length3': 4.86, 'linker_mass3': 101.98, 'σ_3': 0.285, 'e_3': 0.255},
+    5.7: {'linker_mass1': 134.906, 'σ_1': 0.34, 'e_1': 1.2552, 'linker_length2': 5.7, 'linker_mass2': 134.906, 'σ_2': 0.34,
+        'e_2': 1.2552, 'linker_length3': 5.7, 'linker_mass3': 134.906, 'σ_3': 0.34, 'e_3': 1.2552},
+    6.01: {'linker_mass1': 223.8, 'σ_1': 0.4, 'e_1': 1.8731, 'linker_length2': 6.01, 'linker_mass2': 223.8, 'σ_2': 0.4,
+        'e_2': 1.8731, 'linker_length3': 6.01, 'linker_mass3': 223.8, 'σ_3': 0.4, 'e_3': 1.8731},
+    6.41: {'linker_mass1': 317.8, 'σ_1': 0.367, 'e_1': 2.4267, 'linker_length2': 6.41, 'linker_mass2': 317.8, 'σ_2': 0.367,
+        'e_2': 2.4267, 'linker_length3': 6.01, 'linker_mass3': 223.8, 'σ_3': 0.367, 'e_3': 2.4267}
+}
 
+func1_length = {
+    2.278: {'func1_mass': 1., 'func2_length': 2.278, 'func2_mass': 1., 'func3_length': 2.278, 'func3_mass': 1.},
+    3.54: {'func1_mass': 35.45, 'func2_length': 3.54, 'func2_mass': 35.45, 'func3_length': 3.54, 'func3_mass': 35.45},
+    3.78: {'func1_mass': 15., 'func2_length': 3.78, 'func2_mass': 15., 'func3_length': 3.78, 'func3_mass': 15.},
+    3.85: {'func1_mass': 79.9, 'func2_length': 3.85, 'func2_mass': 79.9, 'func3_length': 3.85, 'func3_mass': 79.9},
+    3.927: {'func1_mass': 16., 'func2_length': 3.927, 'func2_mass': 16., 'func3_length': 3.927, 'func3_mass': 16.},
+    4.093: {'func1_mass': 31., 'func2_length': 4.093, 'func2_mass': 31., 'func3_length': 4.093, 'func3_mass': 31.}
+}
+
+MetalNum = {
+    4: {'ionicRad': 41, 'MetalMass': 9.012},
+    29: {'ionicRad': 71, 'MetalMass': 63.456},
+    12: {'ionicRad': 71, 'MetalMass': 24.305},
+    27: {'ionicRad': 72, 'MetalMass': 58.930},
+    30: {'ionicRad': 74, 'MetalMass': 65.380},
+    25: {'ionicRad': 80, 'MetalMass': 54.938},
+    48: {'ionicRad': 92, 'MetalMass': 112.411}
+}
 
 # %% [markdown]
 # # Define my functions to loop through a varying/increasing training dataset
 
 # %%
-def extract_pca_data_space(MOF_vectors, gas_name, trained_model_for_specific_gas):
 
-    # solution = np.asanyarray(MOF_vectors[['MetalNum', 'linker_length1', 'func1_length']])
-
-    if gas_name == 'propylene':
-        diameter = np.array([4.03])
-        mass = np.array([42.08])
-        ascentricF = np.array([0.142])
-        kdiameter = np.array([4.5])
-    elif gas_name == 'co2':
-        diameter = np.array([3.24])
-        mass = np.array([44.01])
-        ascentricF = np.array([0.225])
-        kdiameter = np.array([3.3])
+def map_values(row):
+    metal_num_info = MetalNum[row['MetalNum']]
+    linker_info = linker_length1[row['linker_length1']]
+    func_info = func1_length[row['func1_length']]
     
-    # Provided dictionaries
-    linker_length1 = {
-        3.66: {'linker_mass1': 83, 'σ_1': 0.325, 'e_1': 0.7112, 'linker_length2': 3.66, 'linker_mass2': 83, 'σ_2': 0.325,
-            'e_2': 0.7112, 'linker_length3': 3.66, 'linker_mass3': 83, 'σ_3': 0.325, 'e_3': 0.7112},
-        4.438: {'linker_mass1': 81, 'σ_1': 0.25, 'e_1': 0.0627, 'linker_length2': 4.438, 'linker_mass2': 81, 'σ_2': 0.25,
-                'e_2': 0.0627, 'linker_length3': 4.438, 'linker_mass3': 81, 'σ_3': 0.25, 'e_3': 0.0627},
-        4.86: {'linker_mass1': 101.98, 'σ_1': 0.285, 'e_1': 0.255, 'linker_length2': 4.86, 'linker_mass2': 101.98, 'σ_2': 0.285,
-            'e_2': 0.255, 'linker_length3': 4.86, 'linker_mass3': 101.98, 'σ_3': 0.285, 'e_3': 0.255},
-        5.7: {'linker_mass1': 134.906, 'σ_1': 0.34, 'e_1': 1.2552, 'linker_length2': 5.7, 'linker_mass2': 134.906, 'σ_2': 0.34,
-            'e_2': 1.2552, 'linker_length3': 5.7, 'linker_mass3': 134.906, 'σ_3': 0.34, 'e_3': 1.2552},
-        6.01: {'linker_mass1': 223.8, 'σ_1': 0.4, 'e_1': 1.8731, 'linker_length2': 6.01, 'linker_mass2': 223.8, 'σ_2': 0.4,
-            'e_2': 1.8731, 'linker_length3': 6.01, 'linker_mass3': 223.8, 'σ_3': 0.4, 'e_3': 1.8731},
-        6.41: {'linker_mass1': 317.8, 'σ_1': 0.367, 'e_1': 2.4267, 'linker_length2': 6.41, 'linker_mass2': 317.8, 'σ_2': 0.367,
-            'e_2': 2.4267, 'linker_length3': 6.01, 'linker_mass3': 223.8, 'σ_3': 0.367, 'e_3': 2.4267}
-    }
+    return pd.Series({
+        'linker_mass1': linker_info['linker_mass1'],
+        'σ_1': linker_info['σ_1'],
+        'e_1': linker_info['e_1'],
+        'linker_length2': linker_info['linker_length2'],
+        'linker_mass2': linker_info['linker_mass2'],
+        'σ_2': linker_info['σ_2'],
+        'e_2': linker_info['e_2'],
+        'linker_length3': linker_info['linker_length3'],
+        'linker_mass3': linker_info['linker_mass3'],
+        'σ_3': linker_info['σ_3'],
+        'e_3': linker_info['e_3'],
+        'func1_mass': func_info['func1_mass'],
+        'func2_length': func_info['func2_length'],
+        'func2_mass': func_info['func2_mass'],
+        'func3_length': func_info['func3_length'],
+        'func3_mass': func_info['func3_mass'],
+        'ionicRad': metal_num_info['ionicRad'],
+        'MetalMass': metal_num_info['MetalMass']
+    })
 
-    func1_length = {
-        2.278: {'func1_mass': 1., 'func2_length': 2.278, 'func2_mass': 1., 'func3_length': 2.278, 'func3_mass': 1.},
-        3.54: {'func1_mass': 35.45, 'func2_length': 3.54, 'func2_mass': 35.45, 'func3_length': 3.54, 'func3_mass': 35.45},
-        3.78: {'func1_mass': 15., 'func2_length': 3.78, 'func2_mass': 15., 'func3_length': 3.78, 'func3_mass': 15.},
-        3.85: {'func1_mass': 79.9, 'func2_length': 3.85, 'func2_mass': 79.9, 'func3_length': 3.85, 'func3_mass': 79.9},
-        3.927: {'func1_mass': 16., 'func2_length': 3.927, 'func2_mass': 16., 'func3_length': 3.927, 'func3_mass': 16.},
-        4.093: {'func1_mass': 31., 'func2_length': 4.093, 'func2_mass': 31., 'func3_length': 4.093, 'func3_mass': 31.}
-    }
 
-    MetalNum = {
-        4: {'ionicRad': 41, 'MetalMass': 9.012},
-        29: {'ionicRad': 71, 'MetalMass': 63.456},
-        12: {'ionicRad': 71, 'MetalMass': 24.305},
-        27: {'ionicRad': 72, 'MetalMass': 58.930},
-        30: {'ionicRad': 74, 'MetalMass': 65.380},
-        25: {'ionicRad': 80, 'MetalMass': 54.938},
-        48: {'ionicRad': 92, 'MetalMass': 112.411}
-    }
-
-    # Function to map values from dictionaries to create additional columns
-    def map_values(row):
-        metal_num_info = MetalNum[row['MetalNum']]
-        linker_info = linker_length1[row['linker_length1']]
-        func_info = func1_length[row['func1_length']]
-        
-        return pd.Series({
-            'linker_mass1': linker_info['linker_mass1'],
-            'σ_1': linker_info['σ_1'],
-            'e_1': linker_info['e_1'],
-            'linker_length2': linker_info['linker_length2'],
-            'linker_mass2': linker_info['linker_mass2'],
-            'σ_2': linker_info['σ_2'],
-            'e_2': linker_info['e_2'],
-            'linker_length3': linker_info['linker_length3'],
-            'linker_mass3': linker_info['linker_mass3'],
-            'σ_3': linker_info['σ_3'],
-            'e_3': linker_info['e_3'],
-            'func1_mass': func_info['func1_mass'],
-            'func2_length': func_info['func2_length'],
-            'func2_mass': func_info['func2_mass'],
-            'func3_length': func_info['func3_length'],
-            'func3_mass': func_info['func3_mass'],
-            'ionicRad': metal_num_info['ionicRad'],
-            'MetalMass': metal_num_info['MetalMass']
-        })
-
+def get_complete_vectors(MOF_vectors, gas_name):
     # Apply the mapping function to create new columns in the DataFrame
     new_columns = MOF_vectors.apply(map_values, axis=1)
 
@@ -144,6 +129,17 @@ def extract_pca_data_space(MOF_vectors, gas_name, trained_model_for_specific_gas
         'linker_mass1', 'linker_mass2', 'linker_mass3',
         'func1_length', 'func2_length', 'func3_length', 
         'func1_mass', 'func2_mass', 'func3_mass']]
+    
+    if gas_name == 'propylene':
+        diameter = np.array([4.03])
+        mass = np.array([42.08])
+        ascentricF = np.array([0.142])
+        kdiameter = np.array([4.5])
+    elif gas_name == 'co2':
+        diameter = np.array([3.24])
+        mass = np.array([44.01])
+        ascentricF = np.array([0.225])
+        kdiameter = np.array([3.3])
 
     # Add new columns to the DataFrame with constant values
     new_df['diameter'] = diameter[0]
@@ -153,16 +149,40 @@ def extract_pca_data_space(MOF_vectors, gas_name, trained_model_for_specific_gas
 
     # print("Number of NaN or infinite values in new_df:", np.sum(np.isnan(new_df)) + np.sum(np.isinf(new_df)))
     print("Number of unique rows in new_df:", len(np.unique(new_df, axis=0)))
+    # Print shape before dropping duplicates
+    print("Shape before dropping duplicates:", new_df.shape)
+    # Drop duplicate rows
     new_df=new_df.drop_duplicates()
+    # Print shape after dropping duplicates
+    print("Shape after dropping duplicates:", new_df.shape)
+    
     MOF_and_gas_vector=new_df.values
+    return MOF_and_gas_vector, new_df
+
+def extract_pca_data_space(MOF_vectors, gas_name, trained_model_for_specific_gas):
+
+    # solution = np.asanyarray(MOF_vectors[['MetalNum', 'linker_length1', 'func1_length']])
+
+    if gas_name == 'propylene':
+        diameter = np.array([4.03])
+        mass = np.array([42.08])
+        ascentricF = np.array([0.142])
+        kdiameter = np.array([4.5])
+    elif gas_name == 'co2':
+        diameter = np.array([3.24])
+        mass = np.array([44.01])
+        ascentricF = np.array([0.225])
+        kdiameter = np.array([3.3])
+    
     # MOFs=MOF_vectors[['MetalNum', 'linker_length1', 'func1_length']].drop_duplicates()
-    MOFs = new_df
+    MOF_and_gas_vector, MOFs = get_complete_vectors(MOF_vectors, gas_name)
     # After creating MOF_and_gas_vector, add the following code to check for NaN or infinite values
     print("Number of NaN or infinite values in MOF_and_gas_vector:", np.sum(np.isnan(MOF_and_gas_vector)) + np.sum(np.isinf(MOF_and_gas_vector)))
 
     # Before applying PCA, add the following code to scale the data and check for NaN or infinite values again
     X_scaled = StandardScaler().fit_transform(MOFs)
     print("Number of NaN or infinite values in X_scaled:", np.sum(np.isnan(X_scaled)) + np.sum(np.isinf(X_scaled)))
+    new_df=get_complete_vectors(MOF_vectors, gas_name)[1]
     duplicates_mask = new_df.duplicated()
     print("Number of duplicates in MOF_and_gas_vector:", np.sum(duplicates_mask))   
     # Apply PCA to the input data
@@ -182,6 +202,7 @@ def extract_pca_data_space(MOF_vectors, gas_name, trained_model_for_specific_gas
     print("Number of NaN or infinite values in MOF_and_gas_vector_in_PCA_space:", np.sum(np.isnan(MOF_and_gas_vector_in_PCA_space)) + np.sum(np.isinf(MOF_and_gas_vector_in_PCA_space)))
     print("First few rows of MOF_and_gas_vector_in_PCA_space:")
     print(MOF_and_gas_vector_in_PCA_space[:5])
+    
     # Create DataFrame for PCA components
     final_data_pca = pd.DataFrame(columns=['PC1', 'PC2'])
     final_data_pca['PC1'] = MOF_and_gas_vector_in_PCA_space[:, 0]
@@ -279,7 +300,11 @@ def get_exhaustive_search_data():
 # %%
 import scipy.interpolate
 
-def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, number, trained_model_for_specific_gas, pca_obj, filename = None ):
+def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, gas_name, number, trained_model_for_specific_gas, pca_obj, filename = None ):
+    # Get exhaustive data values and limits
+    exhaustive_search_data = get_exhaustive_search_data()
+    exhaustive_search_data_in_PCA_space = pca_obj.transform(get_complete_vectors((exhaustive_search_data), gas_name))
+
     # Extract the PCA components as a 2D array
     pca_array = pca_df[['PC1', 'PC2']].values
 
@@ -289,6 +314,7 @@ def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, num
     # Create a figure and axes
     fig, ax = plt.subplots()
 
+    # TODO: Remove?
     # Plot the scatter plot of PCA axes with larger point size and thin edge lines
     scatter = ax.scatter(pca_array[:, 0], pca_array[:, 1], c=output_property, cmap='coolwarm', s=100, linewidths=0.2, edgecolors='black')
 
@@ -298,10 +324,10 @@ def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, num
     plt.title(title, fontsize=12)
 
     # Find minima, maxima
-    minXVal = pca_array[:, 0].min() - 0.2
-    maxXVal = pca_array[:, 0].max() + 0.2
-    minYVal = pca_array[:, 1].min() - 0.2
-    maxYVal = pca_array[:, 1].max() + 0.2
+    minXVal = exhaustive_search_data_in_PCA_space[:, 0].min() - 0.2
+    maxXVal = exhaustive_search_data_in_PCA_space[:, 0].max() + 0.2
+    minYVal = exhaustive_search_data_in_PCA_space[:, 1].min() - 0.2
+    maxYVal = exhaustive_search_data_in_PCA_space[:, 1].max() + 0.2
 
     # Increase the scale of both axes
     plt.xlim(minXVal, maxXVal)
@@ -319,30 +345,38 @@ def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, num
     min_output_property = output_property.min()
     max_output_property = output_property.max() 
 
-    # Perform the interpolation using the same range as the original points
-    X = pca_array[:, 0]
-    Y = pca_array[:, 1]
-    Z = output_property
-
-    interp_X = np.linspace(min(X), max(X), num=500)
-    interp_Y = np.linspace(min(Y), max(Y), num=500)
-    gridX, gridY = np.meshgrid(interp_X, interp_Y)  # 2D grid for interpolation
-
 
     # filling in the grid points with either interpolation or predictions from the trained ML model
     if interpolated:
+        # Perform the interpolation using the same range as the original points
+        X = pca_array[:, 0]
+        Y = pca_array[:, 1]
+        Z = output_property
+
+        interp_X = np.linspace(min(X), max(X), num=500)
+        interp_Y = np.linspace(min(Y), max(Y), num=500)
+        gridX, gridY = np.meshgrid(interp_X, interp_Y)  # 2D grid to be filled either by interpolation OR ML predictions
+
         interp = scipy.interpolate.LinearNDInterpolator(list(zip(X, Y)), Z)
         interpZ = interp(gridX, gridY)
     else:        
-        # Transform the grid points back to the original feature space using the PCA inverse_transform function
-        grid_points_in_original_space = pca_obj.inverse_transform(np.column_stack((gridX.ravel(), gridY.ravel())))
+        # For every instance of the exhaustive search space, predict the output
+        predicted_logD = trained_model_for_specific_gas.predict(exhaustive_search_data)
+        X = exhaustive_search_data_in_PCA_space[:, 0]
+        Y = exhaustive_search_data_in_PCA_space[:, 1]
+        Z = predicted_logD
+        # OBSOLETE: Transform the grid points back to the original feature space using the PCA inverse_transform function
+        # grid_points_in_original_space = pca_obj.inverse_transform(np.column_stack((gridX.ravel(), gridY.ravel())))
 
-        # Use the trained model to predict the output property (logD) for the grid points
-        predicted_logD = trained_model_for_specific_gas.predict(grid_points_in_original_space)
+        interp_X = np.linspace(min(X), max(X), num=500)
+        interp_Y = np.linspace(min(Y), max(Y), num=500)
+        gridX, gridY = np.meshgrid(interp_X, interp_Y)  # 2D grid to be filled either by interpolation OR ML predictions
 
-        # Reshape the predicted_logD to match the shape of gridX and gridY for plotting
-        predicted_logD = predicted_logD.reshape(gridX.shape)
-        interpZ = predicted_logD
+        interp = scipy.interpolate.LinearNDInterpolator(list(zip(X, Y)), Z)
+        interpZ = interp(gridX, gridY)
+
+        # Also paint the known points
+        scatterExhaustive = ax.scatter(X, Y, c=predicted_logD, cmap='coolwarm', s=10, linewidths=0.2, edgecolors='black')
  
 
     # Plot the interpolated heatmap
@@ -383,10 +417,10 @@ def plot_pca_data(pca_df, title, randomized_order: bool, interpolated: bool, num
 # # Define the Final Function: run'n'plot
 
 # %%
-def run_n_plot(order: bool, predict_or_no: bool, step= 20, output_dir="output_data"):
+def run_n_plot(gas_name, order: bool, interpolation: bool, step= 20, output_dir="output_data"):
     numbers_list = list(range(step, 111, step))
     OrderRandom = order
-    Interpolation = predict_or_no
+    Interpolation = interpolation
 
     trained_model = None
     pca_obj = None
@@ -413,7 +447,7 @@ def run_n_plot(order: bool, predict_or_no: bool, step= 20, output_dir="output_da
             B='Prediction'
         
         filename = f'{output_dir}/interpZ_values_{A}{B}_{number}.npy'
-        plot_pca_data(pca_df, f'{A}{B} - %{number} of original set', OrderRandom, Interpolation, number, trained_model, pca_obj, filename)
+        plot_pca_data(pca_df, f'{A}{B} - %{number} of original set', OrderRandom, Interpolation, gas_name, number, trained_model, pca_obj, filename)
 
 # %%
-run_n_plot(True, False, 20, output_dir="output_data")
+run_n_plot("co2", True, False, 20, output_dir="output_data")
