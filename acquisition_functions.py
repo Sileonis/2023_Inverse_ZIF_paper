4a4c29c9073776ce54b57e82d021755a679f99dd
vGkatsis <vgkatsis@gmail.com>
2024-02-23 15:18:38 +0200

    Refactored from standalone functions to class.

--- acquisition_functions.py
+++ acquisition_functions.py
@@ -1,44 +1,45 @@
 import numpy as np
 from scipy.stats import norm
+from abc import ABC
 
-# Expected Improvement
-def expected_improvement(x, trainLabels, model, best_y, factor = 2.0):
-    """ Functionality of the Expected Improvement acquisition function.
-       This function estimates how much will a data point improve a model if it is added next to the training dataset.
-       Parameters:
-       x:           The set of datapoints that is available for selection.
-       trainLabels: The set of features intended to be used for training.
-       model:       The model to be trained (usually a Gaussian Process)
-       best_y:      The best target value found so far. (Could be minimum or maximum depending on the problem.)
-       factor:      A factor used as a weight on the exploration-exploitation tradeoff."""
+class AcquisitionCalculator(ABC):
+    def get_acquisition_function(x, featureNames, model, best_y, factor):
+        pass
+    
+class ExpectedImprovementCalculator(AcquisitionCalculator):
+   def __init__(self, factor):
+      self.factor = factor
 
-    x_train = x[trainLabels].to_numpy()
+   def get_acquisition_function(self, x, featureNames, model, best_y):
+      return self.expected_improvement(x, featureNames, model, best_y, self.factor)
+    
+        
+   # Expected Improvement
+   def expected_improvement(x, featureNames, model, best_y, factor = 2.0):
+      """ Functionality of the Expected Improvement acquisition function.
+         This function estimates how much will a data point improve a model if it is added next to the training dataset.
+         Parameters:
+         x:           The dataframe that contains candidate instances to select.
+         featureNames: The names of the features contained in dataframe x, which will be used to create the training vectors.
+         model:       The trained model which maps instances in the x[featureNames] subspace to an estimate of the target variable y (usually a Gaussian Process)
+         best_y:      The best target value y found so far. (Could be minimum or maximum depending on the problem.)
+         factor:      A factor used as a weight on the exploration-exploitation tradeoff."""
 
-    y_pred, y_std = model.predict(x_train, return_std=True)
-    # y_std  = y_pred.std()
+      x_train = x[featureNames].to_numpy()
+
+      y_pred, y_std = model.predict(x_train, return_std=True)
+
+      # Maximization Problem
+      # z = np.divide(np.subtract(y_pred, best_y + factor), y_std)
+      # ei = (np.subtract(y_pred, best_y + factor) * norm.cdf(z)) + (y_std * norm.pdf(z))
+
+      # Minimization Problem
+      z = np.divide(np.subtract(best_y - factor, y_pred), y_std)
+      ei = (np.subtract(best_y - factor, y_pred + factor) * norm.cdf(z)) + (y_std * norm.pdf(z))
 
 
-    # Maximization Problem
-    # z = np.divide(np.subtract(y_pred, best_y + factor), y_std)
-    # ei = (np.subtract(y_pred, best_y + factor) * norm.cdf(z)) + (y_std * norm.pdf(z))
+      # Expeted Improvement Considering the mean score of a ZIF for all gasses.
+      x["expectedImprovement"] = ei.tolist()
 
-    # Minimization Problem
-    z = np.divide(np.subtract(best_y - factor, y_pred), y_std)
-    ei = (np.subtract(best_y - factor, y_pred + factor) * norm.cdf(z)) + (y_std * norm.pdf(z))
-
-
-    # Expeted Improvement Considering the mean score of a ZIF for all gasses.
-    x["expectedImprovement"] = ei.tolist()
-    allZIFs = x.type.unique()
-
-    # meanExpectedImprovement = {}
-    # for zif in allZIFs:
-    #     meanExpectedImprovement[zif] = x[x['type'] == zif].expectedImprovement.sum() / len(x[x["type"] == zif])
-
-    #     # Expected Improvement Considering The Sum of Scores for All Gasses
-    #     # meanExpectedImprovement[zif] = x[x['type'] == zif].expectedImprovement.sum()
-
-    # return ei, max(meanExpectedImprovement, key=meanExpectedImprovement.get)
-
-    # Expected Improvement Considering the gass as a zif feature
-    return ei, x.iloc[np.argmax(ei)]['type']
+      # Expected Improvement Considering the gass as a zif feature
+      return ei, x.iloc[np.argmax(ei)]['type']
