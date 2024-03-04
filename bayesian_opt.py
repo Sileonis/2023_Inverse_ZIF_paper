0478b1f4cf117ceb01616ee5d81fd31317306fa6
vGkatsis <vgkatsis@gmail.com>
2024-03-04 11:36:27 +0200

    Minor fix in expected improvement class arguments.

--- bayesian_opt.py
+++ bayesian_opt.py
@@ -157,7 +157,7 @@ def bayesianOptimization(zifs : pd.DataFrame, X_featureNames : list, Y_featureNa
                 selectRandomSample = False
             else:
                 # Calculate the expected improvement values for all candidate zifs
-                eiCalculator = ExpectedImprovementCalculator()
+                eiCalculator = ExpectedImprovementCalculator(factor=0)
                 eI = eiCalculator.get_acquisition_function(trainZIFs, X_featureNames, gp_model, minMae,0.0)
 
                 # Select the next zif in a greedy manner
