f07138b22d1589f492024901493aba32b1a67733
vGkatsis <vgkatsis@gmail.com>
2024-03-20 10:35:29 +0200

    Plot statistical test restuls in the plot.

--- plot_optimization.py
+++ plot_optimization.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, label1 = '', label2 = '', label3 = '', on_off = 'False', x_min=0, x_max=75, y_min=0, y_max=10, 
+def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, bo_v_random_stats = None, bo_v_serial_stats = None, label1 = '', label2 = '', label3 = '', on_off = 'False', x_min=0, x_max=75, y_min=0, y_max=10, 
                size='16', line=2.5, edge=2, axes_width = 2, tickWidth = 2, tickLength=12, 
             xLabel = '', yLabel ='', fileName = 'picture.png', marker_colors = ['y', 'g', 'r']):
     
@@ -41,6 +41,28 @@ def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, label1 =
         error3 = frame3['stdErrorOfMeanError']
         plt.errorbar(x3, y3, yerr=error3, label=label3, ecolor='k', fmt='o', c=marker_colors[2], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
 
+    stat_test_text = ""
+    if bo_v_random_stats is not None:
+        stat_test_text = "\n".join((stat_test_text,
+                                    "Bayesian vs Random",
+                                    " ".join(("P-Value:",    "{:.3e}".format(bo_v_random_stats["pvalue"]))),
+                                    " ".join(("Stat score:", "{:.3e}".format(bo_v_random_stats["statistic"])))))
+
+    if bo_v_serial_stats is not None:
+        stat_test_text = "\n".join((stat_test_text,
+                                    "\n",
+                                    "Bayesian vs Serial",
+                                    " ".join(("P-Value:",    "{:.3e}".format(bo_v_serial_stats["pvalue"]))),
+                                    " ".join(("Stat score:", "{:.3e}".format(bo_v_serial_stats["statistic"])))))
+
+    if stat_test_text != "":
+        ax =plt.subplot()
+        plt.text(0.83, 0.8, stat_test_text,
+        fontsize = 14,
+        bbox = dict(facecolor='none', edgecolor='grey', pad=5.0),
+        horizontalalignment='left',
+        verticalalignment='top',
+        transform = ax.transAxes)
 
     plt.xlabel(xLabel, fontsize=size)
     plt.ylabel(yLabel, fontsize=size)
