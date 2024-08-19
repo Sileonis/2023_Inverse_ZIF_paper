21bb5939635f17a359f74a17b5f8ff0ad7585fe7
vGkatsis <vgkatsis@gmail.com>
2024-08-19 18:22:17 +0300

    Splitted optimization plots in half in order for results to be more clear.

--- Bayesian_Optimization/plot_optimization.py
+++ Bayesian_Optimization/plot_optimization.py
@@ -27,8 +27,33 @@ def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, method1_
     y1 = frame1['averageError']
     error1 = frame1['stdErrorOfMeanError']
 
-    plt.errorbar(x1, y1, yerr=error1, label=label1, ecolor='k', fmt='o', c=marker_colors[0], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+    division_point = int(len(x1)/2)
+
+    x1_left  = x1.loc[0:division_point]
+    x1_right = x1.loc[division_point + 1:]
+
+    y1_left  = y1.loc[0:division_point]
+    y1_right = y1.loc[division_point + 1:]
+
+    error1_left  = error1.loc[0:division_point]
+    error1_right = error1.loc[division_point + 1:]
+
+    low_y1_right  = round(min(y1_right),1)
+    high_y1_right = round(max(y1_right) + 1, 1)
+    fourth_step   = round((high_y1_right - low_y1_right) / 4, 1)
+
+    plt.subplots(2,1)
+
+    plt.subplot(2,1,1)
+    plt.errorbar(x1_left, y1_left, yerr=error1_left, label=label1, ecolor='k', fmt='o', c=marker_colors[0], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
     plt.legend(loc='upper right', fontsize=15, frameon=on_off)
+
+    plt.subplot(2,1,2)
+    plt.errorbar(x1_right, y1_right, yerr=error1_right, label=label1, ecolor='k', fmt='o', c=marker_colors[0], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+    plt.legend(loc='upper right', fontsize=15, frameon=on_off)
+
+    plt.xticks(np.arange(division_point, max(x1) + 5, 5.0))
+    plt.yticks(np.arange(low_y1_right, high_y1_right, fourth_step))
 
     # Second Method
     if frame2 is not None:
@@ -36,12 +61,39 @@ def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, method1_
         y2 = frame2['averageError']
         error2 = frame2['stdErrorOfMeanError']
 
+        x2_left  = x2[0:division_point]
+        x2_right = x2[division_point:]
+
+        y2_left  = y2.loc[0:division_point]
+        y2_right = y2.loc[division_point + 1:]
+
+        error2_left  = error2.loc[0:division_point]
+        error2_right = error2.loc[division_point + 1:]
+
+        low_y2_right  = round(min(y2_right),1)
+        
+        high_y2_right = round(max(y2_right) + 1, 1)
+
+        if high_y1_right > high_y2_right:
+            high_y2_right = high_y1_right
+        
+        fourth_step   = round((high_y2_right - low_y2_right) / 4, 1)
+
         method1_v_method2_text = "\n".join((" ".join(("P-Value:     ", "{:.3e}".format(method1_v_method2_stats["pvalue"]))),
                                             " ".join(("Stat score:"  , "{:.3e}".format(method1_v_method2_stats["statistic"])))))
 
-        plt.errorbar(x2, y2, yerr=error2, label=label2, ecolor='k', fmt='o', c=marker_colors[1], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+        plt.subplot(2,1,1)
+        plt.errorbar(x2_left, y2_left, yerr=error2_left, label=label2, ecolor='k', fmt='o', c=marker_colors[1], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
         plt.errorbar([ ], [ ], None, label=method1_v_method2_text, linestyle='None')
         plt.legend(loc='upper right', fontsize=15, frameon=on_off)
+
+        plt.subplot(2,1,2)
+        plt.errorbar(x2_right, y2_right, yerr=error2_right, label=label2, ecolor='k', fmt='o', c=marker_colors[1], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+        plt.errorbar([ ], [ ], None, label=method1_v_method2_text, linestyle='None')
+        plt.legend(loc='upper right', fontsize=15, frameon=on_off)
+
+        plt.xticks(np.arange(division_point, max(x2) + 5, 5.0))
+        plt.yticks(np.arange(low_y2_right, high_y2_right, fourth_step))
 
     # Third Method
     if frame3 is not None:
@@ -52,15 +104,43 @@ def plot_logD_trainSize_perMethod(frame1, frame2 = None, frame3 = None, method1_
         method1_v_method3_text = "\n".join((" ".join(("P-Value:     ",    "{:.3e}".format(method1_v_method3_stats["pvalue"]))),
                                             " ".join(("Stat score:", "{:.3e}".format(method1_v_method3_stats["statistic"])))))
 
-        plt.errorbar(x3, y3, yerr=error3, label=label3, ecolor='k', fmt='o', c=marker_colors[2], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+        x3_left  = x3[0:division_point]
+        x3_right = x3[division_point:]
+
+        y3_left  = y3.loc[0:division_point]
+        y3_right = y3.loc[division_point + 1:]
+
+        error3_left  = error3.loc[0:division_point]
+        error3_right = error3.loc[division_point + 1:]
+
+        low_y3_right  = round(min(y3_right), 1)
+
+        high_y3_right = round(max(y3_right) + 1, 1)
+
+        if high_y1_right > high_y3_right:
+            high_y3_right = high_y1_right
+
+        if high_y2_right > high_y3_right:
+            high_y3_right = high_y2_right
+
+        fourth_step   = round((high_y3_right - low_y3_right) / 4, 1)
+
+        plt.subplot(2,1,1)
+        plt.errorbar(x3_left, y3_left, yerr=error3_left, label=label3, ecolor='k', fmt='o', c=marker_colors[2], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
         plt.errorbar([ ], [ ], None, label=method1_v_method3_text, linestyle='None')
         plt.legend(loc='upper right', fontsize=15, frameon=on_off)
     
+        plt.subplot(2,1,2)
+        plt.errorbar(x3_right, y3_right, yerr=error3_right, label=label2, ecolor='k', fmt='o', c=marker_colors[2], markersize=size, linewidth=line, markeredgecolor='k', markeredgewidth=edge)
+        plt.errorbar([ ], [ ], None, label=method1_v_method2_text, linestyle='None')
+        plt.legend(loc='upper right', fontsize=15, frameon=on_off)
+
+        plt.xticks(np.arange(division_point, max(x3) + 5, 5.0))
+        plt.yticks(np.arange(low_y3_right, high_y3_right, fourth_step))
+
 
     plt.tick_params(which='both', width=tickWidth)
     plt.tick_params(which='major', length=tickLength)
-    # plt.yticks(np.arange(min(y1), max(y1), 0.05 * round(max(y1) * 0.2 / 0.05)))
-    plt.xticks(np.arange(0, max(x1) + 5, 5.0))
 
     plt.savefig(fileName, bbox_inches='tight')
     plt.show()
