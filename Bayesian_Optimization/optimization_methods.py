0033f1eb945c82f44020662ffe5913379911b03a
vGkatsis <vgkatsis@gmail.com>
2024-08-20 13:01:28 +0300

    Added dot product kernels for gp model

--- Bayesian_Optimization/optimization_methods.py
+++ Bayesian_Optimization/optimization_methods.py
@@ -19,6 +19,8 @@ from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process.kernels import ConstantKernel
 from sklearn.gaussian_process.kernels import Matern
 from sklearn.gaussian_process.kernels import RationalQuadratic
+from sklearn.gaussian_process.kernels import DotProduct
+from sklearn.gaussian_process.kernels import WhiteKernel
 from sklearn.model_selection import KFold
 
 # TODO Refactor this file by separating functions
@@ -46,10 +48,12 @@ class BayesianOptimization(OptimizationFactory):
         # Initiate a gaussian process model
         #self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: ConstantKernel(1.0) * RBF(1.0) ----------")
         #gp_model = GaussianProcessRegressor(kernel=ConstantKernel(1.0) * RBF(1.0))
-        #self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: 1.0 * Matern(length_scale=1.0, nu=1.5) ----------")
-        #gp_model = GaussianProcessRegressor(kernel=1.0 * Matern(length_scale=1.0, nu=1.5))
-        self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: RationalQuadratic(length_scale=1.0, alpha=1.5) ----------")
-        gp_model = GaussianProcessRegressor(kernel=RationalQuadratic(length_scale=1.0, alpha=1.5))
+        # self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: 1.0 * Matern(length_scale=1.0, nu=2.5) ----------")
+        # gp_model = GaussianProcessRegressor(kernel=1.0 * Matern(length_scale=1.0, nu=2.5))
+        # self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: RationalQuadratic(length_scale=1.0, alpha=1.5) ----------")
+        # gp_model = GaussianProcessRegressor(kernel=RationalQuadratic(length_scale=1.0, alpha=1.5))
+        self.logger.info(self.logPrefix, "----------   Using Gaussian Process Regressor as Surrogate with kernel: DotProduct(sigma_0=1.0) + WhiteKernel() ----------")
+        gp_model = GaussianProcessRegressor(kernel=DotProduct(sigma_0=1.0) + WhiteKernel())
 
         # Count the total number that the kfold process takes in seconds
         total_kfold_elapsed_time = 0.0
